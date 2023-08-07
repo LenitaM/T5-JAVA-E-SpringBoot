4ce285d7c575642b3ac3e5dcb2e559c9fcd7dc4f
LenitaM <lenitamaciel.ti@gmail.com>
2023-08-07 18:07:32 -0300

    primeiros comandos do mysql

---
+++ mysql/CriandoBancoDeDados.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE sucos; /*Este é o comando principal para criação de um banco de dados.*/
+
+DROP DATABASE sucos; /*comando para excluir um bando de dados*/
+
+
---
+++ mysql/conhecendo-workbench.sql
@@ -0,0 +1,7 @@
+/* Conhecendo a Workbench*/
+SELECT * FROM  CITY; /*select * from city. Neste comando estamos selecionando (select) todos os campos (*) (asterisco) da tabela city.*/
+/*Vamos analisar melhor mais para frente o que isso significa, mas apenas exemplificando para facilitar a compreensão: insira no espaço em branco o comando select * from city e em seguida dê um duplo clique no banco de dados world para rodar, note que ficou em negrito (bold),
+ isso quer dizer que os comandos inseridos na área de código estão relacionados ao banco de dados selecionado.*/
+ 
+ SELECT * FROM COUNTRY;
+

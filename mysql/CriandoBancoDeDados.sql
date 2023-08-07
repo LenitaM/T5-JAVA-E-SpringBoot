06e3d0c2ca9a1c2b650e39e457890cd7f558ad25
LenitaM <lenitamaciel.ti@gmail.com>
2023-08-07 18:24:29 -0300

    utilizando o BD no CMD

--- mysql/CriandoBancoDeDados.sql
+++ mysql/CriandoBancoDeDados.sql
@@ -2,4 +2,4 @@ CREATE DATABASE sucos; /*Este é o comando principal para criação de um banco 
 
 DROP DATABASE sucos; /*comando para excluir um bando de dados*/
 
-
+USE SUCOS2; /*para entrar no no BD*/

4a4c6d9f2a85215523ed36963c022b36984ca0eb
LenitaM <lenitamaciel.ti@gmail.com>
2023-08-10 22:18:19 -0300

    alterando dados da tabela

---
+++ mysql/AlterandoRegistros.sql
@@ -0,0 +1,40 @@
+/*Alterando registros*/
+USE sucos2;
+
+INSERT INTO tbProduto (
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PRECO_LISTA)
+VALUES (
+'544931', 
+'Frescor Verão',
+'PET',
+'350ml',
+'Limão',
+3.20);
+
+INSERT INTO tbProduto (
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PRECO_LISTA)
+VALUES (
+'1078680', 
+'Frescor Verão',
+'Lata',
+'470ml',
+'Manga',
+5.18);
+
+SELECT * FROM tbProduto;
+
+/*Sintaxe de alterações de registros nome da coluna = novo dado, WHERE especifica qual produto foi feita a modificação*/
+UPDATE tbProduto SET EMBALAGEM = 'Lata', PRECO_LISTA = 2.46
+WHERE PRODUTO = '544931';
+
+UPDATE tbProduto SET EMBALAGEM = 'Garrafa' WHERE PRODUTO ='1078680'; 
